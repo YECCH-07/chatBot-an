bb00988fa01a3149ff0af3682354be9c5288da0f
YECCH-07 <184193@UNSAAC.EDU.PE>
2025-02-07 17:44:58 -0500

    se estan añadiendo mas controladores

---
+++ src/services/chatwootService.js
@@ -0,0 +1,8 @@
+class ChatwootService {
+    async processEvent(event) {
+        // Implement your event processing logic here
+        console.log('Processing Chatwoot event:', event);
+    }
+}
+
+export default new ChatwootService();
